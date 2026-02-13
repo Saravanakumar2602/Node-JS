16efabe6c0b5dd3b446b418c5288e3b2b987ef5a
Saravanakumar2602 <saravanakumar26022007@gmail.com>
2026-02-13 17:55:40 +0530

    Modules

---
+++ 1.ModulesAndRequire/data.js
@@ -0,0 +1,24 @@
+names = ["saravana","kumar","vaish"];
+ages = [18,17,23];
+
+// console.log(names);
+
+// console.log("Hello Saravana");
+
+// module.exports = names;
+// module.exports = "saravana";
+
+module.exports = {
+    AllNames : names,
+    Allages : ages
+};
+
+
+// module.exports = {
+//      names,ages
+// };
+
+
+
+
+
---
+++ 1.ModulesAndRequire/modules.js
@@ -0,0 +1,11 @@
+// const copy = require("./data");
+
+// console.log(copy);
+// console.log(copy.AllNames);
+
+
+const {AllNames} = require("./data");
+console.log(AllNames);
+
+
+// when we need access from the other files , we use require and at the same time we need to export the data in the required file
