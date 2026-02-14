9d286eeaf257c502b9f901b3f175a594a312c4ba
Saravanakumar2602 <saravanakumar26022007@gmail.com>
2026-02-14 13:01:03 +0530

    npm and dependencies

---
+++ 7.npm-and-dependencies/Server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+const _ = require('lodash');
+
+const server = http.createServer((req, res) => {
+
+    res.setHeader('Content-Type', 'text/html');
+    let path = './docs/';
+
+    if (req.url === '/') {
+        path += 'index.html';
+    } else if(req.url === '/home'){
+        res.statusCode = 301;
+        res.setHeader('Location','/')
+        res.end();
+    }
+    else if (req.url === '/about') {
+        path += 'about.html';
+    } 
+    else if (req.url === '/contact') {
+        path += 'contact.html';
+    } 
+    else {
+        path += '404.html';
+        res.statusCode = 404;
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end('<h1>Internal Server Error</h1>');
+        } else {
+            res.end(data);
+        }
+    });
+
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log("server is listening");
+    console.log(_.random(15,25));
+});
